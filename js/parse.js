85ea003cdaa2c005fb295d52aa806d3d81d78f21
webchick <webchick@24967.no-reply.drupal.org>
2013-04-21 14:31:47 -0700

    Get nodes from front page view.

--- js/parse.js
+++ js/parse.js
@@ -1,9 +1,43 @@
-// Get list of nodes from front page.
-xmlHttp = new XMLHttpRequest();
-xmlHttp.open('GET', '../node/rest', false);
-xmlHttp.setRequestHeader('Accept', 'application/hal+json');
-xmlHttp.send();
-json = xmlHttp.response;
+
+var nodes = getFrontPage();
+console.log(nodes);
+
+/**
+ * Get list of nodes from front page view.
+ *
+ * @todo Generalize this for any REST export view.
+ *
+ * @return array
+ *   Array of JSON objects containing nodes.
+ */
+function getFrontPage() {
+  // Get list of nodes from front page.
+  xmlHttp = new XMLHttpRequest();
+  xmlHttp.open('GET', '../node/rest', false);
+  xmlHttp.setRequestHeader('Accept', 'application/hal+json');
+  xmlHttp.send();
+
+  // Check for common gotchas.
+  if (xmlHttp.status == 404) {
+    console.log('Did you forget to turn on REST module?');
+    console.log('And did you add a "REST export" display on the front page view with a path of "node/rest"?');
+    return false;
+  }
+  else if (xmlHttp.status == 500) {
+    console.log('Did you remember to turn on the HAL module?');
+    return false;
+  }
+  else if (xmlHttp.status == 200) {
+    // OMG it worked! Let's grab us some JSON.
+    nodes = JSON.parse(xmlHttp.response);
+    if (nodes.length == 0) {
+      console.log('Oops. Did you create any nodes?');
+    }
+    return nodes;
+  }
+}
+
+/*
 results = JSON.parse(json);
 
 // Print list of nodes.
@@ -20,3 +54,4 @@ function printNode(node) {
   console.log(node.title[0].value);
   console.log(node);
 }
+*/
