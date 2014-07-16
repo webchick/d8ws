865d75adac0de0f79d1cfa3ea590c117b81ded2e
webchick <drupal@webchick.net>
2014-07-16 06:39:41 -0400

    Dynamic List page coming from Drupal.

--- app.js
+++ app.js
@@ -11,3 +11,40 @@ $(function () {
 
     $.mobile.paramsHandler.init();
 });
+
+// On List page, call out to Drupal to get the list of nodes.
+$(document).on("pageinit", "#list", function( event ) {
+  $.ajax({
+    url: "http://8.x.local:8083/node",
+    type: "GET", // Default.
+    // Add custom header for HAL.
+    beforeSend: function( jqXHR ) {
+      jqXHR.setRequestHeader('Accept', 'application/hal+json');
+    }
+  })
+  .done(function( data ) {
+    var items = [];
+
+    // Request was successful; loop through each result.
+    $(data).each(function( key, value ) {
+      var title = $(data)[key]["title"][0]["value"];
+
+      // Get the node ID of the record from the end of the URL.
+      // @todo: There *has* to be a better way than this. :P
+      var url = $(data)[key]["_links"]["self"]["href"];
+      var nid = url.substring(url.lastIndexOf('/') + 1);
+
+      // Build an array of rows.
+      items.push('<li><a href="#view?id=' + nid + '">' + title + '</a></li>');
+    });
+
+    // Add rows to the listview.
+    $("#content-list").append( items.join('') );
+
+    // Refresh the listview to style the new rows.
+    $("#content-list").listview("refresh");
+  })
+  .fail(function() {
+    alert( "error" );
+  });
+});
